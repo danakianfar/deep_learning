fb4b9fa1451ae15c58bd3fa4a75952bdf0f42fcd
Dana Kianfar <danakianfar@users.noreply.github.com>
2017-11-07 22:46:01 +0100

    MLP definition, layers, feed-forward

--- assignment_1/mlp_numpy.py
+++ assignment_1/mlp_numpy.py
@@ -4,143 +4,240 @@ This module implements a multi-layer perceptron in NumPy.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import numpy as np
+
+
+class Layer(object):
+    """
+    A layer object that handles feed-forward and back propagation ops."""
+
+    def __init__(self, W, b, activation, k):
+        self.W = W
+        self.b = b
+        self.activation = activation
+        self.k = k
+
+    def forward(self, Z):
+        assert Z.shape[0] == self.W.shape[1]
+
+        return np.dot(self.W, Z) + self.b
+
+    def backward(self, delta):
+        # compute gradients
+
+        # apply update
+
+        raise NotImplementedError
+
+    def _update(self, grads, eta):
+        assert grads.shape == self.W.shape, 'Gradient shape {} must match W shape {}'.format(
+            grads.shape, self.W.shape)
+        self.W -= eta * grads
+
+    # keras style printing
+    def __repr__(self):
+        return "W_{} : {} x {}".format(self.k, *self.W.shape)
+
 
 class MLP(object):
-  """
-  This class implements a Multi-layer Perceptron in NumPy.
-  It handles the different layers and parameters of the model.
-  Once initialized an MLP object can perform inference, training and it
-  can also be used for evaluating prediction performance.
-  """
-
-  def __init__(self, n_hidden, n_classes, weight_decay=0.0, weight_scale=0.0001):
     """
-    Constructor for an MLP object. Default values should be used as hints for
-    the usage of each parameter. Weights of the linear layers should be initialized
-    using normal distribution with mean = 0 and std = weight_scale. Biases should be
-    initialized with constant 0. All activation functions are ReLUs.
-
-    Args:
-      n_hidden: list of ints, specifies the number of units
-                     in each hidden layer. If the list is empty, the MLP
-                     will not have any hidden units, and the model
-                     will simply perform a multinomial logistic regression.
-      n_classes: int, number of classes of the classification problem.
-                      This number is required in order to specify the
-                      output dimensions of the MLP.
-      weight_decay: L2 regularization parameter for the weights of linear layers.
-      weight_scale: scale of normal distribution to initialize weights.
-
-    """
-    self.n_hidden = n_hidden
-    self.n_classes = n_classes
-    self.weight_decay = weight_decay
-    self.weight_scale = weight_scale
-
-  def inference(self, x):
-    """
-    Performs inference given an input array. This is the central portion
-    of the network. Here an input array is transformed through application
-    of several hidden layer transformations (as defined in the constructor).
-    We recommend you to iterate through the list self.n_hidden in order to
-    perform the sequential transformations in the MLP. Do not forget to
-    add a linear output layer (without non-linearity) as the last transformation.
-
-    It can be useful to save some intermediate results for easier computation of
-    gradients for backpropagation during training.
-    Args:
-      x: 2D float array of size [batch_size, input_dimensions]
-
-    Returns:
-      logits: 2D float array of size [batch_size, self.n_classes]. Returns
-             the logits outputs (before softmax transformation) of the
-             network. These logits can then be used with loss and accuracy
-             to evaluate the model.
+    This class implements a Multi-layer Perceptron in NumPy.
+    It handles the different layers and parameters of the model.
+    Once initialized an MLP object can perform inference, training and it
+    can also be used for evaluating prediction performance.
     """
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+    def __init__(self,
+                 n_hidden,
+                 n_classes,
+                 weight_decay=0.0,
+                 weight_scale=0.0001):
+        """
+        Constructor for an MLP object. Default values should be used as hints for
+        the usage of each parameter. Weights of the linear layers should be initialized
+        using normal distribution with mean = 0 and std = weight_scale. Biases should be
+        initialized with constant 0. All activation functions are ReLUs.
 
-    return logits
+        Args:
+          n_hidden: list of ints, specifies the number of units
+                         in each hidden layer. If the list is empty, the MLP
+                         will not have any hidden units, and the model
+                         will simply perform a multinomial logistic regression.
+          n_classes: int, number of classes of the classification problem.
+                          This number is required in order to specify the
+                          output dimensions of the MLP.
+          weight_decay: L2 regularization parameter for the weights of linear layers.
+          weight_scale: scale of normal distribution to initialize weights.
 
-  def loss(self, logits, labels):
-    """
-    Computes the multiclass cross-entropy loss from the logits predictions and
-    the ground truth labels. The function will also add the regularization
-    loss from network weights to the total loss that is return.
+        """
+        self.n_hidden = n_hidden
+        self.n_classes = n_classes
+        self.weight_decay = weight_decay
+        self.weight_scale = weight_scale
+        self.input_dim = 3 * 32 * 32
 
-    It can be useful to compute gradients of the loss for an easier computation of
-    gradients for backpropagation during training.
+        # infer shapes of each layer: dimension-first notation W.shape=(D_k+1, D_k)
+        W_shapes = [self.input_dim] + n_hidden + [self.n_classes]
+        W_shapes = [(W_shapes[i + 1], W_shapes[i]) for i in range(len(W_shapes) - 1)]
+        bias_shapes = [(shape[0],1) for shape in W_shapes]
 
-    Args:
-      logits: 2D float array of size [batch_size, self.n_classes].
-                   The predictions returned through self.inference.
-      labels: 2D int array of size [batch_size, self.n_classes]
-                   with one-hot encoding. Ground truth labels for each
-                   sample in the batch.
-    Returns:
-      loss: scalar float, full loss = cross_entropy + reg_loss
-    """
+        # initialize weights
+        self.weights = [
+            self._get_init_weight(shape, self.weight_scale) for shape in W_shapes
+        ]
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+        # initialize biases
+        self.biases = [self._get_bias(dim) for dim in bias_shapes]
 
-    return loss
+        def relu(x):
+            return x * (x > 0)
 
-  def train_step(self, loss, flags):
-    """
-    Implements a training step using a parameters in flags.
-    Use mini-batch Stochastic Gradient Descent to update the parameters of the MLP.
+        def linear(x):
+            return x
 
-    Args:
-      loss: scalar float.
-      flags: contains necessary parameters for optimization.
-    Returns:
+        self.activations = [relu for _ in n_hidden] + [linear]
 
-    """
+        # layer wrapper objects
+        self.layers = [
+            Layer(W=W, b=b, activation=a, k=i)
+            for i, (W, b, a) in enumerate(
+                list(zip(self.weights, self.biases, self.activations)))
+        ]
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+    def _get_init_weight(self, shape, weight_scale):
+        return np.random.normal(0, weight_scale, shape)
 
-    return
+    def _get_bias(self, shape, epsilon=1e-3):
+        return np.zeros(shape=shape) + epsilon
 
-  def accuracy(self, logits, labels):
-    """
-    Computes the prediction accuracy, i.e. the average of correct predictions
-    of the network.
+    def inference(self, x):
+        """
+        Performs inference given an input array. This is the central portion
+        of the network. Here an input array is transformed through application
+        of several hidden layer transformations (as defined in the constructor).
+        We recommend you to iterate through the list self.n_hidden in order to
+        perform the sequential transformations in the MLP. Do not forget to
+        add a linear output layer (without non-linearity) as the last transformation.
 
-    Args:
-      logits: 2D float array of size [batch_size, self.n_classes].
-                   The predictions returned through self.inference.
-      labels: 2D int array of size [batch_size, self.n_classes]
-                 with one-hot encoding. Ground truth labels for
-                 each sample in the batch.
-    Returns:
-      accuracy: scalar float, the accuracy of predictions,
-                i.e. the average correct predictions over the whole batch.
-    """
+        It can be useful to save some intermediate results for easier computation of
+        gradients for backpropagation during training.
+        Args:
+          x: 2D float array of size [batch_size, input_dimensions]
 
-    ########################
-    # PUT YOUR CODE HERE  #
-    #######################
-    raise NotImplementedError
-    ########################
-    # END OF YOUR CODE    #
-    #######################
+        Returns:
+          logits: 2D float array of size [batch_size, self.n_classes]. Returns
+                 the logits outputs (before softmax transformation) of the
+                 network. These logits can then be used with loss and accuracy
+                 to evaluate the model.
+        """
 
-    return accuracy
+        self.results = []
+
+        # dim-first convention
+        Z = x.T
+
+        # feed-forward through each layer and keep results
+        for layer in self.layers:
+            Z = layer.forward(Z)
+            self.results.append(Z)
+
+        logits = self.results[-1]
+
+        return logits
+
+    def loss(self, logits, labels):
+        """
+        Computes the multiclass cross-entropy loss from the logits predictions and
+        the ground truth labels. The function will also add the regularization
+        loss from network weights to the total loss that is return.
+
+        It can be useful to compute gradients of the loss for an easier computation of
+        gradients for backpropagation during training.
+
+        Args:
+          logits: 2D float array of size [batch_size, self.n_classes].
+                       The predictions returned through self.inference.
+          labels: 2D int array of size [batch_size, self.n_classes]
+                       with one-hot encoding. Ground truth labels for each
+                       sample in the batch.
+        Returns:
+          loss: scalar float, full loss = cross_entropy + reg_loss
+        """
+
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        raise NotImplementedError
+        ########################
+        # END OF YOUR CODE    #
+        #######################
+
+        return loss
+
+    def train_step(self, loss, flags):
+        """
+        Implements a training step using a parameters in flags.
+        Use mini-batch Stochastic Gradient Descent to update the parameters of the MLP.
+
+        Args:
+          loss: scalar float.
+          flags: contains necessary parameters for optimization.
+        Returns:
+
+        """
+
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        raise NotImplementedError
+        ########################
+        # END OF YOUR CODE    #
+        #######################
+
+        return
+
+    def accuracy(self, logits, labels):
+        """
+        Computes the prediction accuracy, i.e. the average of correct predictions
+        of the network.
+
+        Args:
+          logits: 2D float array of size [batch_size, self.n_classes].
+                       The predictions returned through self.inference.
+          labels: 2D int array of size [batch_size, self.n_classes]
+                     with one-hot encoding. Ground truth labels for
+                     each sample in the batch.
+        Returns:
+          accuracy: scalar float, the accuracy of predictions,
+                    i.e. the average correct predictions over the whole batch.
+        """
+
+        ########################
+        # PUT YOUR CODE HERE  #
+        #######################
+        raise NotImplementedError
+        ########################
+        # END OF YOUR CODE    #
+        #######################
+
+        return accuracy
+
+    def __repr__(self):
+        sep = '\n|' + '--' * 15 + '\n|\t'
+
+        defs = ['X : {} x batch_size'.format(3 * 32 * 32)] + [str(layer) for layer in self.layers] + [
+            'y:{}'.format(self.n_classes)]
+
+        return sep.join(['|\tNetwork Overview'] + defs) + '\n' + '--'*15
+
+
+def test():
+    net = MLP(n_hidden=[100, 200], n_classes=10)
+    print(net)
+
+    X = np.random.rand(256, 3*32*32)
+    logits = net.inference(X)
+    print(logits.shape)
+
+if __name__ == '__main__':
+    test()
--- assignment_1/train_mlp_numpy.py
+++ assignment_1/train_mlp_numpy.py
@@ -22,67 +22,71 @@ DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
 
 FLAGS = None
 
+
 def train():
-  """
-  Performs training and evaluation of MLP model. Evaluate your model on the whole test set each 100 iterations.
-  """
-  ### DO NOT CHANGE SEEDS!
-  # Set the random seeds for reproducibility
-  np.random.seed(42)
+    """
+    Performs training and evaluation of MLP model. Evaluate your model on the whole test set each 100 iterations.
+    """
+    ### DO NOT CHANGE SEEDS!
+    # Set the random seeds for reproducibility
+    np.random.seed(42)
 
-  ## Prepare all functions
-  # Get number of units in each hidden layer specified in the string such as 100,100
-  if FLAGS.dnn_hidden_units:
-    dnn_hidden_units = FLAGS.dnn_hidden_units.split(",")
-    dnn_hidden_units = [int(dnn_hidden_unit_) for dnn_hidden_unit_ in dnn_hidden_units]
-  else:
-    dnn_hidden_units = []
+    ## Prepare all functions
+    # Get number of units in each hidden layer specified in the string such as 100,100
+    if FLAGS.dnn_hidden_units:
+        dnn_hidden_units = FLAGS.dnn_hidden_units.split(",")
+        dnn_hidden_units = [int(dnn_hidden_unit_) for dnn_hidden_unit_ in dnn_hidden_units]
+    else:
+        dnn_hidden_units = []
 
-  ########################
-  # PUT YOUR CODE HERE  #
-  #######################
-  raise NotImplementedError
-  ########################
-  # END OF YOUR CODE    #
-  #######################
+    ########################
+    # PUT YOUR CODE HERE  #
+    #######################
+    raise NotImplementedError
+    ########################
+    # END OF YOUR CODE    #
+    #######################
+
 
 def print_flags():
-  """
-  Prints all entries in FLAGS variable.
-  """
-  for key, value in vars(FLAGS).items():
-    print(key + ' : ' + str(value))
+    """
+    Prints all entries in FLAGS variable.
+    """
+    for key, value in vars(FLAGS).items():
+        print(key + ' : ' + str(value))
+
 
 def main():
-  """
-  Main function
-  """
-  # Print all Flags to confirm parameter settings
-  print_flags()
+    """
+    Main function
+    """
+    # Print all Flags to confirm parameter settings
+    print_flags()
 
-  if not os.path.exists(FLAGS.data_dir):
-    os.makedirs(FLAGS.data_dir)
+    if not os.path.exists(FLAGS.data_dir):
+        os.makedirs(FLAGS.data_dir)
 
-  # Run the training operation
-  train()
+    # Run the training operation
+    train()
+
 
 if __name__ == '__main__':
-  # Command line arguments
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--dnn_hidden_units', type = str, default = DNN_HIDDEN_UNITS_DEFAULT,
-                      help='Comma separated list of number of units in each hidden layer')
-  parser.add_argument('--learning_rate', type = float, default = LEARNING_RATE_DEFAULT,
-                      help='Learning rate')
-  parser.add_argument('--max_steps', type = int, default = MAX_STEPS_DEFAULT,
-                      help='Number of steps to run trainer.')
-  parser.add_argument('--batch_size', type = int, default = BATCH_SIZE_DEFAULT,
-                      help='Batch size to run trainer.')
-  parser.add_argument('--weight_init_scale', type = float, default = WEIGHT_INITIALIZATION_SCALE_DEFAULT,
-                      help='Weight initialization scale (e.g. std of a Gaussian).')
-  parser.add_argument('--weight_reg_strength', type = float, default = WEIGHT_REGULARIZER_STRENGTH_DEFAULT,
-                      help='Regularizer strength for weights of fully-connected layers.')
-  parser.add_argument('--data_dir', type = str, default = DATA_DIR_DEFAULT,
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dnn_hidden_units', type=str, default=DNN_HIDDEN_UNITS_DEFAULT,
+                        help='Comma separated list of number of units in each hidden layer')
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE_DEFAULT,
+                        help='Learning rate')
+    parser.add_argument('--max_steps', type=int, default=MAX_STEPS_DEFAULT,
+                        help='Number of steps to run trainer.')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE_DEFAULT,
+                        help='Batch size to run trainer.')
+    parser.add_argument('--weight_init_scale', type=float, default=WEIGHT_INITIALIZATION_SCALE_DEFAULT,
+                        help='Weight initialization scale (e.g. std of a Gaussian).')
+    parser.add_argument('--weight_reg_strength', type=float, default=WEIGHT_REGULARIZER_STRENGTH_DEFAULT,
+                        help='Regularizer strength for weights of fully-connected layers.')
+    parser.add_argument('--data_dir', type=str, default=DATA_DIR_DEFAULT,
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
 
-  main()
+    main()
